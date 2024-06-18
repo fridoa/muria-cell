53887184ef02b108c430bb487c71683f1a10b51e
Frido Afriyanto <146181205+fridoa@users.noreply.github.com>
2024-06-18 19:46:44 +0700

    Update koneksi.php

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,7 +1,14 @@
 <?php
-$koneksi = mysqli_connect("localhost","root","","db_dpw1");
+$db_name = "mysql:host=db;dbname=db_dpw1";
+$user_name ='root';
+$user_password = 'admin';
 
-if(mysqli_connect_errno()){
-    echo "Koneksi database gagal: " . mysqli_connect_error();
+try {
+        $koneksi = new PDO($db_name, $user_name, $user_password);
+        $koneksi -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException  $e){
+        echo "Jepat njirr: " . $e->getMessage();
 }
+
+$email = mysqli_real_escape_string($mysqli, 'admin@gmail.com'); 
 ?>
